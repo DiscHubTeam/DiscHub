6b44f00564971056f11d393d3b441954a8cc2f99
rodTrodd <rod.trodd@gmail.com>
2017-12-29 17:18:44 +0000

    Password Checking and Clean-Up
    Added logic to ensure passwords match each other, feedback provided through string properties - worth noting for later! Also removed unused code in create account class due to copy-and-past pro-forma from different class.

--- DiscHubApp/src/main/java/com/dischub/views/CreateAccountPresenter.java
+++ DiscHubApp/src/main/java/com/dischub/views/CreateAccountPresenter.java
@@ -5,12 +5,15 @@ import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.dischub.DiscHub;
 import com.dischub.login.UserCredentials;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -28,22 +31,13 @@ public class CreateAccountPresenter extends GluonPresenter<DiscHub> {
     private TextField txfScreenName;
 
     @FXML
+    private Label lblInfo;
+
+    @FXML
     private PasswordField pwdPassword;
 
     @FXML
     private PasswordField pwdConfirmPassword;
-
-    @FXML
-    private Button btnLogin;
-
-    @FXML
-    private Button btnCreateAccount;
-
-    @FXML
-    private Button btnResetPassword;
-
-    @FXML
-    private VBox vbxContainer;
 
     @FXML
     private ResourceBundle resources;
@@ -51,10 +45,10 @@ public class CreateAccountPresenter extends GluonPresenter<DiscHub> {
     private String encryptedPasswordString;
 
     public void initialize() {
+
         primary.showingProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue) {
                 AppBar appBar = getApp().getAppBar();
-                appBar.setVisible(false);
                 appBar.setTitleText("Login");
                 appBar.setVisible(false);
             }
@@ -65,13 +59,9 @@ public class CreateAccountPresenter extends GluonPresenter<DiscHub> {
     void buttonClick() {
         if (isValidUserName(txfEmail.getText())) {
             if (isPasswordValid(pwdPassword.getText())) {
-                DiscHub.setUserCredentials(new UserCredentials(txfEmail.getText(), encryptedPasswordString, "Rowan"/*db.query*/, 0));
                 createAccount();
-
-                //AppViewManager.PRIMARY_VIEW.switchView();
             }
         }
-
     }
 
     private void createAccount() {
@@ -79,7 +69,6 @@ public class CreateAccountPresenter extends GluonPresenter<DiscHub> {
         System.out.println("Requested create new login \n Name: " + txfScreenName.getText() + " \n uname: " + txfEmail.getText() + " \n Password: " + pwdPassword.getText());
         DiscHub.setUserCredentials(new UserCredentials(txfEmail.getText(), pwdPassword.getText(), txfScreenName.getText(), 0));
         showConfirmation();
-        System.out.println("Showing Confirmation");
     }
 
     private boolean isValidUserName(String text) {
@@ -88,8 +77,13 @@ public class CreateAccountPresenter extends GluonPresenter<DiscHub> {
     }
 
     private boolean isPasswordValid(String text) {
-        //query datatbase
-        return true;
+        if (pwdPassword.getText().equals(pwdConfirmPassword.getText())) {
+            return true;
+        } else {
+            lblInfo.setText(resources.getString("label.text.4"));
+            return false;
+        }
+
     }
 
     private void showConfirmation() {
