80d9ee3386c2b237c46171c7422a24c1c30de653
rodTrodd <rod.trodd@gmail.com>
2017-12-29 13:07:04 +0000

    Added Basis for Create Account Page
    Added Basis for Create Account Page. No true controls added, but again, this will only be a stub until the database design is finalised and the interface outlined clearly.

---
+++ DiscHubApp/src/main/java/com/dischub/views/CreateAccountPresenter.java
@@ -0,0 +1,88 @@
+package com.dischub.views;
+
+import com.gluonhq.charm.glisten.afterburner.GluonPresenter;
+import com.gluonhq.charm.glisten.control.AppBar;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.dischub.DiscHub;
+import com.dischub.login.UserCredentials;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CreateAccountPresenter extends GluonPresenter<DiscHub> {
+
+    @FXML
+    private View primary;
+    
+    @FXML
+    private TextField txfEmail;
+    
+    @FXML
+    private PasswordField pwdPassword;
+    
+    @FXML
+    private PasswordField pwdConfirmPassword;
+    
+    @FXML
+    private Button btnLogin;
+    
+    @FXML
+    private Button btnCreateAccount;
+    
+    @FXML
+    private Button btnResetPassword;
+
+    @FXML
+    private ResourceBundle resources;
+    
+    private String encryptedPasswordString;
+
+    public void initialize() {
+        primary.showingProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue) {
+                AppBar appBar = getApp().getAppBar();
+                appBar.setVisible(false);
+                appBar.setTitleText("Login");
+                appBar.setVisible(false);
+            }
+        });
+    }
+
+    @FXML
+    void buttonClick() {
+        if(isValidUserName(txfEmail.getText())){
+            if(isPasswordValid(pwdPassword.getText())){
+                DiscHub.setUserCredentials(new UserCredentials(txfEmail.getText(), encryptedPasswordString,"Rowan"/*db.query*/, 0));
+                AppViewManager.PRIMARY_VIEW.switchView();
+                
+            }
+        }
+        
+    }
+    
+    @FXML
+    void createAccount(){
+        //Navigate to new screen to make an account.
+        System.out.println("Requested create new login");
+    }
+    
+    @FXML
+    void resetPassword(){
+        //Will need to think of the best way to do that!
+    }
+
+    private boolean isValidUserName(String text) {
+        //Database.query();
+        return true;
+    }
+
+    private boolean isPasswordValid(String text) {
+       //query datatbase
+        return true;
+    }
+    
+    
+
+}
