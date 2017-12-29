ea1bb8a51acce5c0f2f7afcebf760a467aa71eb7
rodTrodd <rod.trodd@gmail.com>
2017-12-29 15:14:45 +0000

    Delay between account creation and login.
    Displaying feedback once account is created and the app logging in. This is done in a small thread which will only run once. Also added different strings to the lang properties. Some H/coded strings have found their way in, but these can be fixed later as its not a priority.

--- DiscHubApp/src/main/java/com/dischub/views/CreateAccountPresenter.java
+++ DiscHubApp/src/main/java/com/dischub/views/CreateAccountPresenter.java
@@ -6,40 +6,48 @@ import com.gluonhq.charm.glisten.mvc.View;
 import com.dischub.DiscHub;
 import com.dischub.login.UserCredentials;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class CreateAccountPresenter extends GluonPresenter<DiscHub> {
 
     @FXML
     private View primary;
-    
+
     @FXML
     private TextField txfEmail;
-    
+
     @FXML
     private TextField txfScreenName;
-    
+
     @FXML
     private PasswordField pwdPassword;
-    
+
     @FXML
     private PasswordField pwdConfirmPassword;
-    
+
     @FXML
     private Button btnLogin;
-    
+
     @FXML
     private Button btnCreateAccount;
-    
+
     @FXML
     private Button btnResetPassword;
 
     @FXML
+    private VBox vbxContainer;
+
+    @FXML
     private ResourceBundle resources;
-    
+
     private String encryptedPasswordString;
 
     public void initialize() {
@@ -55,34 +63,53 @@ public class CreateAccountPresenter extends GluonPresenter<DiscHub> {
 
     @FXML
     void buttonClick() {
-        if(isValidUserName(txfEmail.getText())){
-            if(isPasswordValid(pwdPassword.getText())){
-                DiscHub.setUserCredentials(new UserCredentials(txfEmail.getText(), encryptedPasswordString,"Rowan"/*db.query*/, 0));
+        if (isValidUserName(txfEmail.getText())) {
+            if (isPasswordValid(pwdPassword.getText())) {
+                DiscHub.setUserCredentials(new UserCredentials(txfEmail.getText(), encryptedPasswordString, "Rowan"/*db.query*/, 0));
                 createAccount();
+
                 //AppViewManager.PRIMARY_VIEW.switchView();
-                
             }
         }
-        
+
     }
-        
-    private void createAccount(){
+
+    private void createAccount() {
         //Navigate to new screen to make an account.
-        System.out.println("Requested create new login \n Name: "+txfScreenName.getText()+" \n uname: "+txfEmail.getText()+" \n Password: "+pwdPassword.getText());
+        System.out.println("Requested create new login \n Name: " + txfScreenName.getText() + " \n uname: " + txfEmail.getText() + " \n Password: " + pwdPassword.getText());
         DiscHub.setUserCredentials(new UserCredentials(txfEmail.getText(), pwdPassword.getText(), txfScreenName.getText(), 0));
-        AppViewManager.PRIMARY_VIEW.switchView();
+        showConfirmation();
+        System.out.println("Showing Confirmation");
     }
-    
+
     private boolean isValidUserName(String text) {
         //Database.query();
         return true;
     }
 
     private boolean isPasswordValid(String text) {
-       //query datatbase
+        //query datatbase
         return true;
     }
-    
-    
+
+    private void showConfirmation() {
+        primary.setTop(new Label("Created New Account \nLogging In"));
+        //This thread allows us to wait for 2 seconds before being logged in 
+        //- giving feedback to the user that the account creation was successful
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(CreateAccountPresenter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            Platform.runLater(() -> {
+                AppViewManager.PRIMARY_VIEW.switchView();
+            });
+
+            return;
+        });
+
+        t.start();
+    }
 
 }
